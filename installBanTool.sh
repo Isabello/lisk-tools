19d88ac818ed64e342e284e04b5898dabf4f0dba
Isabello <dell.isabella@gmail.com>
2016-04-30 23:50:31 -0500

    Update installBanTool.sh

--- installBanTool.sh
+++ installBanTool.sh
@@ -6,7 +6,13 @@ sudo ipset create excluded_list hash:net
 
 cd ~
 
-git clone https://github.com/slasheks/lisk-api.git
+wget https://github.com/slasheks/lisk-api/archive/master.zip
+
+unzip master.zip
+
+mv lisk-api-master lisk-api
+
+rm -f master.zip
 
 cd lisk-api/python2/
 
@@ -14,8 +20,14 @@ wget https://raw.githubusercontent.com/vinay20045/json-to-csv/master/json_to_csv
 
 cd ~
 
-git clone https://github.com/isabello/lisk-ban-tool.git
+wget https://github.com/Isabello/Lisk_Management_Tools/archive/master.zip
 
-cd lisk-ban-tool
+unzip master.zip
+
+mv Lisk_Management_Tools-master/ Lisk_Management_Tools
+
+rm -f master.zip
+
+cd Lisk_Management_Tools
 
 mkdir ban_list
