19a49eb1e9c4d287ad7c07c165f56b39d9e73985
Isabello <dell.isabella@gmail.com>
2016-05-01 13:27:54 -0500

    Update installLiskTools.sh
    adding support for user selected locations

--- installLiskTools.sh
+++ installLiskTools.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
+default_tools_location=~
+default_backup_location=~
+
+read -r -p "Where do you want to install to? (Default $default_tools_location):  " tools_location
+tools_location=${tools_location:-$default_tools_location}
+if [[ ! -r "$tools_location" ]]
+then
+echo "$tools_location is not valid, please check and re-excute"
+exit 2;
+fi
+
+
+read -r -p "Where do you want to install to? (Default $default_backup_location):  " backup_location
+backup_location=${backup_location:-$default_backup_location}
+if [[ ! -r "$backup_location" ]]
+then
+echo "$backup_location is not valid, please check and re-excute"
+exit 2;
+fi
+
+read -p "Do you want to install the Ban Tool? Y or N: " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
 sudo apt-get -qqy install ipset
 
 sudo ipset create excluded_list hash:net
 
-cd ~
+cd $tools_location
 
 wget https://github.com/slasheks/lisk-api/archive/master.zip
 
@@ -18,7 +42,9 @@ cd lisk-api/python2/
 
 wget https://raw.githubusercontent.com/vinay20045/json-to-csv/master/json_to_csv.py
 
-cd ~
+fi
+
+cd $tools_location
 
 wget https://github.com/Isabello/Lisk_Management_Tools/archive/master.zip
 
@@ -30,4 +56,5 @@ rm -f master.zip
 
 cd Lisk_Management_Tools
 
-mkdir ban_list
+mkdir -p $tools_location/Lisk_Management_Tools/ban_list
+mkdir -p $backup_location/pg_backups
