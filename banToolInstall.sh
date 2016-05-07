68981c629fa7dd7c8c24b7eb530bd6b95819ee46
Isabello <dell.isabella@gmail.com>
2016-05-07 02:53:34 -0500

    Create banToolInstall.sh

---
+++ banToolInstall.sh
@@ -0,0 +1,23 @@
+#read -p "Do you want to install the Ban Tool? Y or N: " -n 1 -r -i "N"
+#if [[ $REPLY =~ ^[Yy]$ ]]
+#then
+
+#sudo apt-get -qqy install ipset
+
+#sudo ipset create excluded_list hash:net
+
+#cd $tools_location
+
+#wget https://github.com/slasheks/lisk-api/archive/master.zip
+
+#unzip master.zip
+
+#mv lisk-api-master lisk-api
+
+#rm -f master.zip
+
+#cd lisk-api/python2/
+
+#wget https://raw.githubusercontent.com/vinay20045/json-to-csv/master/json_to_csv.py
+
+#fi
