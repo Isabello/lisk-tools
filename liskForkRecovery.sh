38010cfd6530f8d5d3657b3a9d540489c94963d4
Isabello <dell.isabella@gmail.com>
2016-05-07 02:45:32 -0500

    Create liskForkRecovery.sh

---
+++ liskForkRecovery.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+while true; do
+s1=`curl -s "http://127.0.0.1:7000/api/loader/status/sync"| jq '.height'`
+sleep 30
+s2=`curl -s "http://127.0.0.1:7000/api/loader/status/sync"| jq '.height'`
+
+
+if [ "$s1" == "$s2" ]
+then
+echo "$s1" " " "$s2"
+echo "Block Height is hung. Restoring from most recent backup"
+bash tools_location/liskBackupMan.sh autorestore
+sleep 300
+
+else
+echo "" "$s1" " " "$s2"
+echo "everything is fine"
+sleep 10
+fi
+
+done
