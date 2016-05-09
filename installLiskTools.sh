39d8ee76c83157e80a2f75a49779caf3b38a2179
Isabello <dell.isabella@gmail.com>
2016-05-09 12:11:38 -0500

    Update installLiskTools.sh

--- installLiskTools.sh
+++ installLiskTools.sh
@@ -3,7 +3,6 @@
 default_tools_location=~
 default_backup_location=~
 default_lisk_location=~/lisk-0.2.1-Linux-x86_64
-REPLY=Y
 
 read -r -p "Where do you want to install to? (Default $default_tools_location):  " tools_location
 tools_location=${tools_location:-$default_tools_location}
@@ -48,26 +47,7 @@ cd lisk-tools
 mkdir -p $tools_location/lisk-tools/ban_list
 mkdir -p $backup_location/pg_backup
 
-sed -i "s|backup_location|$backup_location|g" "liskBackupMan.sh"
+sed -i "s|backup_home|$backup_location|g" "liskBackupMan.sh"
 sed -i "s|lisk_home|$lisk_location|g" "liskBackupMan.sh"
-sed -i "s|tools_location|$tools_location|g" "liskBackupMan.sh"
+sed -i "s|tools_home|$tools_location|g" "liskBackupMan.sh"
 
-read -r -p -n 1 "Do you want to automatically take backups? (Default Y): " $REPLY
-REPLY=${REPLY:-$REPLY}
-if [[  $REPLY =~ ^[Yy]$ ]]
-then
-bash $tools_location/lisk_tools/liskBackupMan.sh schedule
-fi
-
-read -r -p -n 1 "Do you want to automatically recover from forks? (Default Y): " $REPLY
-REPLY=${REPLY:-$REPLY}
-if [[  $REPLY =~ ^[Yy]$ ]]
-then
-sudo apt-get install -qqy jq curl
-nohup /bin/bash $tools_location/liskForkRecovery.sh 0<&- &>/dev/null &
-cronjob_line="@reboot /bin/bash $tools_location/liskForkRecovery.sh"
-crontab -l | grep -v 'liskForkRecovery.sh'  | crontab - 
-(crontab -l; echo "$cronjob_line" )  | crontab - |  echo "Auto Recovery Configured to run on Reboot"
-fi
-
-sed -i "s|lisk_home|$lisk_location|g" "liskForkRecovery.sh"
